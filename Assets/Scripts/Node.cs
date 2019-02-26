feee58f838079b192be0c2ea053eb22141783b0d
Dan Wareing <daniel.wareing6@gmail.com>
2019-02-26 21:07:59 +0100

    Nodes, Links, and restricted movement along Links

--- Assets/Scripts/Node.cs
+++ Assets/Scripts/Node.cs
@@ -4,18 +4,35 @@ using UnityEngine;
 
 public class Node : MonoBehaviour {
 
+	Vector2 m_coordinate;
+	public Vector2 Coordinate { get { return Utility.Vector2Round(m_coordinate); } }
+	List<Node> m_neighbourNodes = new List<Node>();
+	public List<Node> NeighbourNodes { get { return m_neighbourNodes; } }
+	List<Node> m_linkedNodes = new List<Node>();
+	public List<Node> LinkedNodes { get { return m_linkedNodes; } }
+	Board m_board;
 	public GameObject geometry;
 	public float scaleTime = 0.3f;
 	public iTween.EaseType easeType = iTween.EaseType.easeInExpo;
 	public bool autoRun = false;
 	public float delay = 1f;
+	bool m_isInitialized = false;
+	public GameObject linkPrefab;
 
+	void Awake() {
+		m_board = Object.FindObjectOfType<Board>();
+		m_coordinate = new Vector2(transform.position.x, transform.position.z);
+	}
 	void Start () {
 		if (geometry != null) {
 			geometry.transform.localScale = Vector3.zero;
 			if (autoRun) {
-				ShowGeometry();
+				InitNode();
 			}
+		}
+
+		if (m_board != null) {
+			m_neighbourNodes = FindNeighbours(m_board.AllNodes);
 		}
 	}
 
@@ -30,4 +47,59 @@ public class Node : MonoBehaviour {
 		}
 	}
 	
+	public List<Node> FindNeighbours(List<Node> nodes) {
+		List<Node> nList = new List<Node>();
+		foreach (Vector2 dir in Board.directions) {
+			Node foundNeighbour = nodes.Find(
+				n => n.Coordinate == Coordinate + dir
+			);
+			if (foundNeighbour != null && !nList.Contains(foundNeighbour)) {
+				nList.Add(foundNeighbour);
+			}
+		}
+		return nList;
+	}
+
+	public void InitNode() {
+		if (!m_isInitialized) {
+			ShowGeometry();
+			InitNeighbours();
+		}
+		m_isInitialized = true;
+	}
+
+	void InitNeighbours() {
+		StartCoroutine(InitNeighboursRoutine());
+	}
+
+	IEnumerator InitNeighboursRoutine() {
+		yield return new WaitForSeconds(delay);
+		foreach (Node n in m_neighbourNodes) {
+			if (!m_linkedNodes.Contains(n)) {
+				LinkNode(n);
+				n.InitNode();
+			}
+		}
+	}
+
+	void LinkNode(Node targetNode) {
+		if (linkPrefab != null) {
+			GameObject linkInstance = Instantiate(
+				linkPrefab, transform.position, Quaternion.identity
+			);
+			linkInstance.transform.parent = transform;
+
+			Link link = linkInstance.GetComponent<Link>();
+			if (link != null) {
+				link.DrawLink(transform.position, targetNode.transform.position);
+			}
+
+			if (!m_linkedNodes.Contains(targetNode)) {
+				m_linkedNodes.Add(targetNode);
+			}
+			if (!targetNode.LinkedNodes.Contains(this)) {
+				targetNode.LinkedNodes.Add(this);
+			}
+		}
+	}
 }
--- Assets/Scripts/PlayerMover.cs
+++ Assets/Scripts/PlayerMover.cs
@@ -10,14 +10,25 @@ public class PlayerMover : MonoBehaviour {
 
 	public float moveSpeed = 1.5f;
 	public float iTweenDelay = 0f;
+	Board m_board;
 
 	// Use this for initialization
-	void Start () {
-
+	void Awake () {
+		m_board = Object.FindObjectOfType<Board>().GetComponent<Board>();
 	}
 	
 	public void Move(Vector3 destinationPos, float delayTime = 0.25f) {
-		StartCoroutine(MoveRoutine(destinationPos, delayTime));
+		if (m_board != null) {
+			Debug.Log(destinationPos);
+			Node targetNode = m_board.FindNodeAt(destinationPos);
+			if (targetNode != null) {
+				StartCoroutine(MoveRoutine(destinationPos, delayTime));
+			} else {
+				Debug.Log("Invalid move");
+			}
+		} else {
+			Debug.Log("No board");
+		}
 	}
 
 	IEnumerator MoveRoutine(Vector3 destinationPos, float delayTime) {
@@ -41,22 +52,22 @@ public class PlayerMover : MonoBehaviour {
 	}
 
 	public void MoveLeft() {
-		Vector3 newPosition = transform.position + new Vector3(-2,0,0);
+		Vector3 newPosition = transform.position + new Vector3(-Board.spacing,0f,0f);
 		Move(newPosition,0);
 	}
 
 	public void MoveRight() {
-		Vector3 newPosition = transform.position + new Vector3(2,0,0);
+		Vector3 newPosition = transform.position + new Vector3(Board.spacing,0f,0f);
 		Move(newPosition,0);
 	}
 
 	public void MoveForward() {
-		Vector3 newPosition = transform.position + new Vector3(0,0,2);
+		Vector3 newPosition = transform.position + new Vector3(0f,0f,Board.spacing);
 		Move(newPosition,0);
 	}
 
 	public void MoveBackward() {
-		Vector3 newPosition = transform.position + new Vector3(0,0,-2);
+		Vector3 newPosition = transform.position + new Vector3(0f,0f,-Board.spacing);
 		Move(newPosition,0);
 	}
 }
